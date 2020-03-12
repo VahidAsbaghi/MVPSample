80c0e9c17d101fd8cf701491dbdc354668459d95
VahidAsbaghi <35479944+VahidAsbaghi@users.noreply.github.com>
2020-03-12 20:04:44 +0330

    Add files via upload

---
+++ MVPTest/MVPTest.Presentation/ViewMainPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPTest.Presentation
+{
+    public class ViewMainPresenter:IViewMainPresenter
+    {
+        private readonly IViewMain _view;
+        private readonly IApplicationController _appController;
+
+        public ViewMainPresenter(IViewMain view,IApplicationController appController)
+        {
+            _view = view;
+            _appController = appController;
+            _view.Presenter = this;
+        }
+        public IViewMain View
+        {
+            get { return _view; }
+            
+        }
+
+        public void RunSimpleForm()
+        {
+            _appController.RunSimpleForm();
+        }
+    }
+}
---
+++ MVPTest/MVPTest.Presentation/ViewSimplePresenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPTest.Presentation
+{
+    public class ViewSimplePresenter:IViewSimplePresenter
+    {
+        private readonly IViewSimple _view;
+        private readonly IApplicationController _appController;
+
+        public ViewSimplePresenter(IViewSimple view, IApplicationController appController)
+        {
+            _view = view;
+            _appController = appController;
+            _view.Presenter = this;
+        }
+
+        public IViewSimple View
+        {
+            get { return _view; }
+        }
+    }
+}
